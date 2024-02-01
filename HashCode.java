bb30be5d114aaf7d7481fbd507d90e03eb528522
Prajjwal Sharma <prajjwal.sharma@paynways.com>
2024-02-01 18:32:22 +0530

    update

---
+++ HashCode.java
@@ -0,0 +1,12 @@
+public class HashCode {
+   
+    public static void main(String[] args) {
+        // System.out.println(true);
+        System.out.println("hello prajjwal sharma");
+String a= "prajjwal.sharma@paynways.com";
+System.out.println(a.hashCode());
+
+    }
+
+
+}
